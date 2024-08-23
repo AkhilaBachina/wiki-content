5ab3aa17e5ea4d0819efd0c5cf03002718c1b617
Akhila Bachina (EXT-Nokia) <akhila.bachina.ext@nokia.com>
2024-08-23 12:21:37 +0530

    Initial commit: Adding downloaded wiki content

---
+++ wiki/index890f.php
@@ -0,0 +1,12 @@
+<?xml version="1.0"?>
+<?xml-stylesheet type="text/css" href="http://sugar.fp.nsn-rdnet.net/wiki/skins/common/feed.css?303"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
+		<id>http://sugar.fp.nsn-rdnet.net/wiki/index.php?title=Special:RecentChanges&amp;feed=atom</id>
+		<title>SugarWiki  - Recent changes [en]</title>
+		<link rel="self" type="application/atom+xml" href="http://sugar.fp.nsn-rdnet.net/wiki/index.php?title=Special:RecentChanges&amp;feed=atom"/>
+		<link rel="alternate" type="text/html" href="http://sugar.fp.nsn-rdnet.net/wiki/index.php/Special:RecentChanges"/>
+		<updated>2024-08-23T04:41:31Z</updated>
+		<subtitle>Track the most recent changes to the wiki in this feed.</subtitle>
+		<generator>MediaWiki 1.21.1</generator>
+
+	</feed>
